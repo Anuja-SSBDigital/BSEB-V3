d16ee453f33922edb5aed76114cb683e286d5217
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-02-24 15:27:10 +0530

    Search Button Add

--- Agency/Agencymaster.aspx
+++ Agency/Agencymaster.aspx
@@ -56,6 +56,30 @@
                     <asp:TextBox ID="txtUsername" runat="server" CssClass="form-control" />
                 </div>
 
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="agency-types">Agency Type</label>
+
+                        <asp:DropDownList
+                            runat="server"
+                            ID="ddlagency_type"
+                            CssClass="form-control">
+
+                            <asp:ListItem Value="">Select Agency Types</asp:ListItem>
+                            <asp:ListItem Value="BSEB DATACENTER">BSEB DATACENTER</asp:ListItem>
+                            <asp:ListItem Value="Scanning Agencies">Scanning Agencies</asp:ListItem>
+                            <asp:ListItem Value="Result Processing">Result Processing</asp:ListItem>
+                            <asp:ListItem Value="Marks Entry">Marks Entry</asp:ListItem>
+                            <asp:ListItem Value="Printing">Printing</asp:ListItem>
+
+                        </asp:DropDownList>
+
+                        <div class="invalid-feedback">
+                            Please select Agency Types
+                        </div>
+                    </div>
+                </div>
+
                 <div class="col-md-6 mb-3">
                     <label>Email</label>
                     <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" />
@@ -78,4 +102,135 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Agency Details </h4>
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+
+
+                        <%--   <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="agency-name">Agency Name</label>
+
+                                <asp:DropDownList
+                                    runat="server"
+                                    ID="ddlOwnerAgency"
+                                    CssClass="form-control"
+                                    AppendDataBoundItems="true">
+                                    <asp:ListItem Value="" Text="Select Agency Name"></asp:ListItem>
+                                </asp:DropDownList>
+
+                                <div class="invalid-feedback">Please select Agency Name</div>
+                            </div>
+                        </div>--%>
+
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="agency-name">Agency Status</label>
+                                <asp:DropDownList runat="server" ID="ddl_Agencytatus" CssClass="form-control" Required="true">
+                                    <asp:ListItem Value="ALL" Text="Select Agency status" Selected="True"></asp:ListItem>
+                                    <asp:ListItem Value="Active" Text="Active"></asp:ListItem>
+                                    <asp:ListItem Value="DeActive" Text="DeActive"></asp:ListItem>
+
+                                </asp:DropDownList>
+                                <div class="invalid-feedback">Please select Status</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <asp:Button runat="server" ID="btn_search" Text="Search" CssClass="btn btn-primary btn-lg " OnClick="btn_search_Click" />
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-12" runat="server" id="User_detailes" visible="false">
+            <div class="card card-primary">
+                <div class="card-header">
+
+                    <h4>Agency Details</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped" id="table-1">
+                            <thead>
+                                <tr>
+                                    <th>Sr No</th>
+                                    <th>Agency Name</th>
+                                    <th>User Name</th>
+
+                                    <th>Email</th>
+                                    <th>Mobile No</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <asp:Repeater runat="server" ID="rpt_userData" OnItemCommand="rpt_userData_ItemCommand" OnItemDataBound="rpt_userData_ItemDataBound">
+                                    <ItemTemplate>
+                                        <tr>
+                                            <td>
+                                                <asp:HiddenField runat="server" ID="hf_emailid" Value='<%#Eval("email") %>' />
+                                                <asp:HiddenField runat="server" ID="hf_agency" Value='<%#Eval("agencyname") %>' />
+                                                <asp:HiddenField ID="hf_status" runat="server" Value='<%#Eval("status") %>' />
+                                                <asp:HiddenField runat="server" ID="hf_username" Value='<%#Eval("username") %>' />
+                                                <asp:HiddenField runat="server" ID="hf_userid" Value='<%#Eval("id") %>' />
+
+                                                <asp:Label ID="lblRowNumber" Text='<%# Container.ItemIndex + 1 %>' runat="server" />
+                                            </td>
+
+                                            <td><%#Eval("agencyname") %></td>
+                                            <td><%#Eval("username") %></td>
+
+                                            <td><%#Eval("email") %></td>
+                                            <td><%#Eval("mobileno") %></td>
+                                            <td>
+                                                <asp:Label ID="lbl_userstatus" runat="server" Text='<%#Eval("status") %>'></asp:Label>
+                                            </td>
+                                            <td>
+                                                <asp:LinkButton ID="link_approve" CommandName="link_approve"
+                                                    CommandArgument='<%#Eval("id") %>'
+                                                    runat="server" data-bs-toggle="tooltip" data-placement="right"
+                                                    title="Approve" CssClass="btn-icon btn-success btn-sm "
+                                                    Visible="false" OnClientClick="return confirm('Are you sure you want to Approve This User?');">
+                                                    <i class="fas fa-check"></i>
+                                                </asp:LinkButton>
+                                                &nbsp;
+                                                            <asp:LinkButton ID="link_rejected" CommandName="link_rejected" data-bs-toggle="tooltip" data-placement="right" title="Reject"
+                                                                CommandArgument='<%#Eval("id") %>' runat="server"
+                                                                CssClass="btn-icon btn-danger btn-sm" Visible="false"
+                                                                OnClientClick="return confirm('Are you sure you want to Reject This User?');">
+                                                                <i class="fas fa-times"></i>
+                                                            </asp:LinkButton>
+                                                <asp:LinkButton ID="link_Active" CommandName="link_Active" CommandArgument='<%#Eval("id") %>' runat="server" CssClass="btn-icon btn-success btn-sm" Visible="false">
+                                                    Active
+                                                </asp:LinkButton>
+                                                <asp:LinkButton ID="link_DeActive" CommandName="link_DeActive"
+                                                    CommandArgument='<%#Eval("id") %>' runat="server"
+                                                    CssClass="btn-icon btn-danger btn-sm" Visible="false">
+                                                    Deactive</asp:LinkButton>
+                                            </td>
+
+                                        </tr>
+                                    </ItemTemplate>
+                                </asp:Repeater>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- Agency/Agencymaster.aspx.cs
+++ Agency/Agencymaster.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Cryptography;
@@ -11,7 +12,23 @@ using System.Web.UI.WebControls;
 
 public partial class Agency_Agencymaster : System.Web.UI.Page
 {
+    FlureeCS fl = new FlureeCS();
 
+    protected void Page_Load(object sender, EventArgs e) 
+    {
+        if (!IsPostBack)
+        {
+            if (Session["userid"] != null)
+            {
+              //  BindAgencyDropdown();
+            }
+            else
+            {
+                Response.Redirect("../login.aspx");
+            }
+        }
+    }
+                                                                                                     
     protected void btnSave_Click(object sender, EventArgs e)
     {
         string conStr = ConfigurationManager
@@ -30,7 +47,7 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
 
                 int userExists = (int)checkCmd.ExecuteScalar();
 
-                if (userExists > 0)
+                if (userExists > 0)         
                 {
                     ScriptManager.RegisterStartupScript(this, GetType(),
                         "userExists",
@@ -40,7 +57,15 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
                 }
             }
 
-       
+            if (string.IsNullOrEmpty(ddlagency_type.SelectedValue))
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                    "typeError",
+                    "alert('Please select Agency Type.');",
+                    true);
+                return;
+            }
+
             string plainPassword = GeneratePassword();
 
           
@@ -49,10 +74,10 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
             using (SqlCommand cmd = new SqlCommand(@"
             INSERT INTO agencyuser
             (username, email, mobileno, agencyname, password, role,
-             status, created_at, updated_at, PrivateKey, Key_Expiry, PlainTextPassword)
+             status, created_at, updated_at, PrivateKey, Key_Expiry, PlainTextPassword,AgencyType)
             VALUES
             (@username, @email, @mobileno, @agencyname, @password, @role,
-             'Active', GETDATE(), GETDATE(), '', NULL, @PlainTextPassword)", con))
+             'Active', GETDATE(), GETDATE(), '', NULL, @PlainTextPassword, @AgencyType)", con))
             {
                 cmd.Parameters.AddWithValue("@username", txtUsername.Text.Trim());
                 cmd.Parameters.AddWithValue("@email", txtEmail.Text.Trim());
@@ -62,6 +87,9 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
                 cmd.Parameters.AddWithValue("@PlainTextPassword", plainPassword);
                 cmd.Parameters.AddWithValue("@role", "Agency");
 
+                cmd.Parameters.AddWithValue("@AgencyType", ddlagency_type.SelectedValue);
+
+
                 cmd.ExecuteNonQuery();
             }
 
@@ -69,14 +97,14 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
                             + txtUsername.Text.Trim() +
                             " | Password: " + plainPassword + "');";
 
+          
             ScriptManager.RegisterStartupScript(this, GetType(),
                 "SuccessMessage", script, true);
-
+                
             ClearForm();
         }
-    }        
-
-
+    }            
+       
     public string EncryptString(string str)
     {
         MD5 md5Hash = MD5.Create();
@@ -92,7 +120,6 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
         return sBuilder.ToString();
     }
 
-   
     private string GeneratePassword()
     {
         string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -112,5 +139,146 @@ public partial class Agency_Agencymaster : System.Web.UI.Page
         txtEmail.Text = "";
         txtMobile.Text = "";
         txtAgency.Text = "";
+    }
+
+    protected void btn_search_Click(object sender, EventArgs e)
+    {
+        //DataTable resforuser = fl.FindAgency(ddlOwnerAgency.SelectedValue);
+
+        DataTable resforuser = fl.FindAgencystatus(ddl_Agencytatus.SelectedValue);
+
+
+        if (resforuser.Rows.Count > 0)
+        {
+            User_detailes.Visible = true;
+            rpt_userData.DataSource = resforuser;
+            rpt_userData.DataBind();
+        }
+        else
+        {
+            rpt_userData.DataSource = null;
+            rpt_userData.DataBind();
+        }
+    }
+
+
+    //private void BindAgencyDropdown()
+    //{
+    //    string conStr = ConfigurationManager
+    //                    .ConnectionStrings["dbcon"]
+    //                    .ConnectionString;
+
+    //    using (SqlConnection con = new SqlConnection(conStr))
+    //    {
+    //        using (SqlCommand cmd = new SqlCommand(
+    //            @"SELECT DISTINCT LTRIM(RTRIM(agencyname)) AS agencyname
+    //          FROM agencyuser
+    //          WHERE agencyname IS NOT NULL
+    //          GROUP BY LTRIM(RTRIM(agencyname))
+    //          ORDER BY LTRIM(RTRIM(agencyname))", con))
+    //        {
+    //            con.Open();
+
+    //            ddlOwnerAgency.DataSource = cmd.ExecuteReader();
+    //            ddlOwnerAgency.DataTextField = "agencyname";
+    //            ddlOwnerAgency.DataValueField = "agencyname";
+    //            ddlOwnerAgency.DataBind();
+    //        }
+    //    }
+    //}
+
+    protected void rpt_userData_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            Label lbl_userstatus = (Label)e.Item.FindControl("lbl_userstatus");
+            LinkButton link_Active = (LinkButton)e.Item.FindControl("link_Active");
+            LinkButton link_DeActive = (LinkButton)e.Item.FindControl("link_DeActive");
+            LinkButton link_approve = (LinkButton)e.Item.FindControl("link_approve");
+            LinkButton link_rejected = (LinkButton)e.Item.FindControl("link_rejected");
+            HiddenField hf_status = (HiddenField)e.Item.FindControl("hf_status");
+
+            if (lbl_userstatus.Text == "Active")
+            {
+                link_DeActive.Visible = true;
+                link_Active.Visible = false;
+                link_approve.Visible = false;
+                link_rejected.Visible = false;
+            }
+            else if (lbl_userstatus.Text == "DeActive")
+            {
+                link_Active.Visible = true;
+                link_DeActive.Visible = false;
+                link_approve.Visible = false;
+                link_rejected.Visible = false;
+            }
+            else if (lbl_userstatus.Text == "Rejected")
+            {
+                link_approve.Visible = true;
+                link_rejected.Visible = true;
+                link_Active.Visible = false;
+                link_DeActive.Visible = false;
+            }
+            else
+            {
+                link_approve.Visible = true;
+                link_rejected.Visible = true;
+                link_Active.Visible = false;
+                link_DeActive.Visible = false;
+            }
+
+        }
+
+    }
+            
+    protected void rpt_userData_ItemCommand(object source, RepeaterCommandEventArgs e)
+    {
+        HiddenField emailid = (HiddenField)e.Item.FindControl("hf_emailid");
+        HiddenField agency = (HiddenField)e.Item.FindControl("hf_agency");
+        HiddenField username = (HiddenField)e.Item.FindControl("hf_username");
+
+
+                   
+        if (e.CommandName == "link_approve")
+        {
+            string userId = e.CommandArgument.ToString();
+            string resUser = fl.Updateagencyuserstatus(userId, "Active");
+
+        }
+        else if (e.CommandName == "link_rejected")
+        {
+            string res = fl.Updateagencyuserstatus(e.CommandArgument.ToString(), "Rejected");
+            if (!res.StartsWith("Error"))
+            {
+                fl.log.Info("Status Changed Successfully");
+                Response.Write("<script>alert('Agency Rejected Successfully');location.href = location.href;</script>");
+            }
+        }
+        else if (e.CommandName == "link_Active")
+        {
+            string res = fl.Updateagencyuserstatus(e.CommandArgument.ToString(), "Active");
+            if (!res.StartsWith("Error"))
+            {
+                fl.log.Info("Status Changed Successfully");
+                Response.Write("<script>alert('Agency Status Changed Successfully');location.href = location.href;</script>");
+            }
+        }
+        else if (e.CommandName == "link_DeActive")
+        {
+            string res = fl.Updateagencyuserstatus(e.CommandArgument.ToString(), "DeActive");
+            if (!res.StartsWith("Error"))
+            {
+                fl.log.Info("Status Changed Successfully");
+                Response.Write("<script>alert('Agency Status Changed Successfully');location.href = location.href;</script>");
+            }
+        }
+    }
+            
+    private string GenerateRandomPassword(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
